e481f9f96d5473c8df55faf9a1bce6b8e290b563
cone26 <sight41siyah@gmail.com>
2023-12-15 22:01:15 +0900

    user dto:

--- libs/dao/src/base-time.dto.ts
+++ libs/dao/src/base-time.dto.ts
@@ -1,6 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BaseTimeDto {
   @ApiProperty()
+  createdAt: Date;
 
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  deletedAt: Date;
 }
--- libs/dao/src/common/user/user.dto.ts
+++ libs/dao/src/common/user/user.dto.ts
@@ -1,3 +1,13 @@
-export class UserDto extends{
-  @Api
+import { BaseTimeDto } from '@app/dao/base-time.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserDto extends BaseTimeDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  nickName: string;
+
+  @ApiProperty()
+  gameDbId: number;
 }
