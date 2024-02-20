3d8023350441b4e59276f37009d0132fb4129bee
cone26 <sight41siyah@gmail.com>
2024-02-20 21:56:41 +0900

    typeorm set up

--- libs/common/src/database/typeorm/typeorm-module.option.ts
+++ libs/common/src/database/typeorm/typeorm-module.option.ts
@@ -1,8 +1,10 @@
 import { DataSourceOptions, getMetadataArgsStorage } from 'typeorm';
 import { CustomNamingStrategy } from '@libs/common/database/typeorm/custom-naming-strategy';
+import * as process from 'process';
 
 export const defaultTypeOrmOptions: DataSourceOptions = {
   type: 'mysql',
+  url: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER_NAME,
@@ -10,6 +12,9 @@ export const defaultTypeOrmOptions: DataSourceOptions = {
   synchronize:
     (process.env.DB_SYNCHRONIZE && JSON.parse(process.env.DB_SYNCHRONIZE)) ||
     false,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   namingStrategy: new CustomNamingStrategy(),
   charset: 'utf8mb4',
   timezone: 'Z',
@@ -26,7 +31,7 @@ export const defaultTypeOrmOptions: DataSourceOptions = {
 export const commonTypeOrmModuleOptions: DataSourceOptions = {
   ...defaultTypeOrmOptions,
   name: process.env.DB_COMMON_NAME,
-  database: process.env.DB_COMMON_NAME,
+  database: process.env.DB_NAME,
   entities: ['dist/libs/dao/src/common/**/*.entity.{ts,js}'],
   migrations: ['dist/libs/dao/src/migration/database/common/*.{ts,js}'],
 };
